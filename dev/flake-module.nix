2ef7e7e7a24c0fd792d19aae5fed3cc66a5f0760
Sander <hey@sandydoo.me>
2026-01-09 17:08:32 +0100

    flake: fix devShell on macOS machines

--- dev/flake-module.nix
+++ dev/flake-module.nix
@@ -11,6 +11,7 @@
     {
       config,
       pkgs,
+      lib,
       inputs',
       ...
     }:
@@ -42,52 +43,53 @@
         ''}";
       };
 
-      devShells.default = pkgs.mkShell {
-        name = "nix-bindings-devshell";
-        strictDeps = true;
-        inputsFrom = [ config.nci.outputs.nix-bindings.devShell ];
-        inherit (config.nci.outputs.nix-bindings.devShell.env)
-          LIBCLANG_PATH
-          NIX_CC_UNWRAPPED
-          ;
-        NIX_DEBUG_INFO_DIRS =
-          let
-            # TODO: add to Nixpkgs lib
-            getDebug =
-              pkg:
-              if pkg ? debug then
-                pkg.debug
-              else if pkg ? lib then
-                pkg.lib
-              else
-                pkg;
-          in
-          "${getDebug config.packages.nix}/lib/debug";
-        buildInputs = [
-          config.packages.nix
-        ];
-        nativeBuildInputs = [
-          pkgs.rust-analyzer
-          pkgs.nixfmt-rfc-style
-          pkgs.rustfmt
-          pkgs.pkg-config
-          pkgs.clang-tools # clangd
-          pkgs.valgrind
-          pkgs.gdb
-          pkgs.hci
-          # TODO: set up cargo-valgrind in shell and build
-          #       currently both this and `cargo install cargo-valgrind`
-          #       produce a binary that says ENOENT.
-          # pkgs.cargo-valgrind
-        ];
-        shellHook = ''
-          ${config.pre-commit.installationScript}
-          source ${../bindgen-gcc.sh}
-          echo 1>&2 "Welcome to the development shell!"
-        '';
-        # rust-analyzer needs a NIX_PATH for some reason
-        NIX_PATH = "nixpkgs=${inputs.nixpkgs}";
-      };
+      devShells.default = pkgs.mkShell (
+        config.nci.outputs.nix-bindings.devShell.env
+        // {
+          name = "nix-bindings-devshell";
+          strictDeps = true;
+          inputsFrom = [ config.nci.outputs.nix-bindings.devShell ];
+          NIX_DEBUG_INFO_DIRS =
+            let
+              # TODO: add to Nixpkgs lib
+              getDebug =
+                pkg:
+                if pkg ? debug then
+                  pkg.debug
+                else if pkg ? lib then
+                  pkg.lib
+                else
+                  pkg;
+            in
+            "${getDebug config.packages.nix}/lib/debug";
+          buildInputs = [
+            config.packages.nix
+          ];
+          nativeBuildInputs = [
+            pkgs.rust-analyzer
+            pkgs.nixfmt-rfc-style
+            pkgs.rustfmt
+            pkgs.pkg-config
+            pkgs.clang-tools # clangd
+            pkgs.gdb
+            pkgs.hci
+            # TODO: set up cargo-valgrind in shell and build
+            #       currently both this and `cargo install cargo-valgrind`
+            #       produce a binary that says ENOENT.
+            # pkgs.cargo-valgrind
+          ]
+          ++ lib.optionals pkgs.stdenv.isLinux [
+            pkgs.valgrind
+          ];
+          shellHook = ''
+            ${config.pre-commit.installationScript}
+            source ${../bindgen-gcc.sh}
+            echo 1>&2 "Welcome to the development shell!"
+          '';
+          # rust-analyzer needs a NIX_PATH for some reason
+          NIX_PATH = "nixpkgs=${inputs.nixpkgs}";
+        }
+      );
     };
   herculesCI =
     { config, ... }:
